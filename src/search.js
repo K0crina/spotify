ee78c279c9ca57f0f5678532951d3b09b5b16693
K0crina <crinamiruna2006@gmail.com>
2025-12-11 20:02:35 +0200

    Redesigned search bar section

--- src/search.js
+++ src/search.js
@@ -16,33 +16,46 @@ let current = null;
 
 input.oninput = async () => {
   const q = input.value.trim();
+  
+  // Selectam toate titlurile
+  const titles = document.querySelectorAll(".search-title");
+
   if (!q) {
     tracksBox.innerHTML = "";
     artistsBox.innerHTML = "";
     albumsBox.innerHTML = "";
+    
+    // ASCUNDEM titlurile daca nu e text
+    titles.forEach(h3 => h3.style.display = "none");
     return;
   }
+
+  // Daca avem text, AFISAM titlurile
+  titles.forEach(h3 => h3.style.display = "block");
 
   const data = await searchSpotify(q);
 
   //  MELODII
   tracksBox.innerHTML = data.tracks.items.slice(0, 5).map((t, i) => `
-    <div class="music-card">
-      <strong>#${i + 1}</strong>
-      <img src="${t.album.images?.[0]?.url || ""}">
-      <p>${t.name}</p>
+    <div class="music-card search-card">  <div class="card-info">
+        <strong>#${i + 1}</strong>
+        <img src="${t.album.images?.[0]?.url || ""}">
+        <div class="text-details">
+            <p class="track-name">${t.name}</p>
+            <span class="artist-name">${t.artists[0].name}</span>
+        </div>
+      </div>
       ${
         t.preview_url
-          ? `<button onclick="play('${t.preview_url}')">▶ Play</button>`
+          ? `<button onclick="play('${t.preview_url}')"><i class="fas fa-play"></i></button>`
           : ""
       }
     </div>
   `).join("");
 
-  // ARTIȘTI
+  // ARTISTI
   artistsBox.innerHTML = data.artists.items.slice(0, 5).map((a, i) => `
-    <div class="music-card">
-      <strong>#${i + 1}</strong>
+    <div class="music-card search-card">
       <img src="${a.images?.[0]?.url || ""}">
       <p>${a.name}</p>
     </div>
@@ -50,8 +63,7 @@ input.oninput = async () => {
 
   //  ALBUME
   albumsBox.innerHTML = data.albums.items.slice(0, 5).map((a, i) => `
-    <div class="music-card">
-      <strong>#${i + 1}</strong>
+    <div class="music-card search-card">
       <img src="${a.images?.[0]?.url || ""}">
       <p>${a.name}</p>
     </div>
